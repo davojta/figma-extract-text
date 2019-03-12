325e5945b06d2f835fdec6ee6ce836096f2eae60
Dzianis Sheka <dzianis.sheka@gmail.com>
2019-03-12 08:00:22 +0300

    feat: add index.html with bootsrap starter

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
